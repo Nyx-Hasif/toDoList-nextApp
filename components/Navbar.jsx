b9ac3e632b636389337e7635644b893fc69649d8
Nyx-Hasif <nikaresif97@gmail.com>
2024-11-09 15:45:55 +0800

    updwidth

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Navbar = () => {
   return (
   
-      <div className="flex flex-wrap w-screen overflow-auto  md:w-fit md:flex-col gap-4 fixed bottom-0  md:bottom-0 md:right-0 border border-black px-6 py-4  text-white text-sm md:text-lg font-medium bg-slate-900">
+      <div className="flex flex-wrap w-full overflow-auto  md:w-fit md:flex-col gap-4 fixed bottom-0  md:bottom-0 md:right-0 border border-black px-6 py-4  text-white text-sm md:text-lg font-medium bg-slate-900">
         <Link href="#structure" className="hover:text-yellow-500 ">Structure</Link>
         <Link href="#connectDB" className="hover:text-yellow-500 ">DB Connect</Link>
         <Link href="#model" className="hover:text-yellow-500 ">Model</Link>
